21518d67bd1a8c503e651b10239f66e9d571a1dc
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-09 22:56:26 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_2.0/HomeTask_2.0.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int a,summa = 0;
+    cin >> a;
+    for (int i = a; i <= 500; i++) {
+        summa = summa + i;
+    }
+    cout << summa;
+}
+
